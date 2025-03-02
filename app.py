2719752d6914714dc662c592c5ae4e45a0986939
porgimats <144575966+porgimats@users.noreply.github.com>
2025-03-02 16:25:55 +0000

    First Commmit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from markupsafe import escape
+from flask import url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'index'
+
+@app.route('/login')
+def login():
+    return 'login'
+
+@app.route('/user/<username>')
+def profile(username):
+    return f'{username}\'s profile'
+
+
+
